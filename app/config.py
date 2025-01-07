1770116e30f5d39c65d9cc8d4c411f230d744e7c
paula raafat <paularaafat27@.com>
2025-01-07 16:56:17 +0200

    General modification

--- app/config.py
+++ app/config.py
@@ -5,7 +5,13 @@ load_dotenv()
 
 
 class Config:
-    # General Configurations
+    """General Configurations"""
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
+
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
